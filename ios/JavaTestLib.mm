2e73a065389962b3bf51305c26c7476e1b6796fa
Ahmed Maddeh AZAIEZ <amazaiez@mobelite.fr>
2023-12-11 15:32:40 +0100

    test ios

--- ios/JavaTestLib.mm
+++ ios/JavaTestLib.mm
@@ -1,10 +1,26 @@
+// #import <React/RCTBridgeModule.h>
+
+// @interface RCT_EXTERN_MODULE(JavaTestLib, NSObject)
+
+// RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
+//                  withResolver:(RCTPromiseResolveBlock)resolve
+//                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+// + (BOOL)requiresMainQueueSetup
+// {
+//   return NO;
+// }
+
+// @end
+
+
 #import <React/RCTBridgeModule.h>
 
 @interface RCT_EXTERN_MODULE(JavaTestLib, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(multiply:(NSString *)ch
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
 
 + (BOOL)requiresMainQueueSetup
 {
--- ios/JavaTestLib.swift
+++ ios/JavaTestLib.swift
@@ -1,8 +1,18 @@
+// @objc(JavaTestLib)
+// class JavaTestLib: NSObject {
+
+//   @objc(multiply:withB:withResolver:withRejecter:)
+//   func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+//     resolve(a*b)
+//   }
+// }
+
+
 @objc(JavaTestLib)
 class JavaTestLib: NSObject {
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
+  @objc func multiply(ch: String) -> String {
+    return ch + "test"
 }
+}
+
